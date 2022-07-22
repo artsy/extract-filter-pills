e4e088a7e647aa043e504624825ab1e261e56663
Dima Tretyak <itretyak@ya.ru>
2022-07-22 19:19:47 +0300

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: ["./tsconfig.json"] },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": ["off"]
+  },
+}
